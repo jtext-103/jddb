294204fe0add73c85614e3dfeb7eecfbd72b4f4f
Fengming Xue <1349976364@qq.com>
2023-03-09 17:55:36 +0800

    Rename Signal.prop_dict to Signal.attributes.

--- code/JDDB/jddb/processor/signal.py
+++ code/JDDB/jddb/processor/signal.py
@@ -10,21 +10,21 @@ class Signal(object):
 
     Attributes:
         data (numpy.ndarray): raw data of the Signal instance.
-        prop_dict (dict): necessary attributes of the Signal instance. Note that SampleRate and StartTime will be set default if not given.
+        attributes (dict): necessary attributes of the Signal instance. Note that SampleRate and StartTime will be set default if not given.
         tag (str): name of the signal. Optional.
     """
 
-    def __init__(self, data: np.ndarray, prop_dict, tag: Optional[str] = None):
+    def __init__(self, data: np.ndarray, attributes, tag: Optional[str] = None):
         self.data = data
-        self.prop_dict = prop_dict
+        self.attributes = attributes
         self.tag = tag
 
-        if 'SampleRate' not in self.prop_dict.keys():
-            self.prop_dict['SampleRate'] = 1
+        if 'SampleRate' not in self.attributes.keys():
+            self.attributes['SampleRate'] = 1
             warnings.warn("SampleRate not in props. Set to 1.")
 
-        if 'StartTime' not in self.prop_dict.keys():
-            self.prop_dict['StartTime'] = 0
+        if 'StartTime' not in self.attributes.keys():
+            self.attributes['StartTime'] = 0
             warnings.warn("StartTime not in props. Set to 0.")
 
     @property
@@ -33,7 +33,7 @@ class Signal(object):
         Returns:
             numpy.ndarray: time axis of the Signal instance according to SampleRate and StartTime given in the prop_dict.
         """
-        start_time = self.prop_dict['StartTime']
-        sample_rate = self.prop_dict['SampleRate']
+        start_time = self.attributes['StartTime']
+        sample_rate = self.attributes['SampleRate']
         down_time = start_time + (len(self.data) - 1) / sample_rate
         return np.linspace(start_time, down_time, len(self.data))
