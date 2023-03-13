20cd22c437ec725c07de9a983b00e5792d9effe0
Fengming Xue <1349976364@qq.com>
2023-03-13 13:50:57 +0800

    Function replace_pattern in utils.py.

---
+++ code/JDDB/jddb/utils.py
@@ -0,0 +1,20 @@
+import re
+
+
+def replace_pattern(directory: str, shot: int) -> str:
+    """Replace the '$shot_*$' pattern to normal directory to save shots.
+
+    Args:
+        directory (str): origin input directory that may cover the pattern.
+        shot (int): the shot number to save.
+    Returns:
+        str: replaced directory to save shots.
+    """
+    pattern = r'\$shot_\d+\$'
+    match = re.findall(pattern, directory)
+    if match:
+        for each in match:
+            number = 10 ** int(re.findall(r'\d+', each)[0])
+            directory = directory.replace(each, '{}'.format(int(shot) // number))
+
+    return directory
