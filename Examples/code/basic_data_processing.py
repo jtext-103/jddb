c77e2f57d912838026a119432af6aec27360030d
scs <353067236@qq.com>
2023-03-14 17:15:10 +0800

    fix some typo

--- Examples/code/basic_data_processing.py
+++ Examples/code/basic_data_processing.py
@@ -2,12 +2,12 @@
 
 # In this program you are show how to build a data processing pipeline
 
-# this pipeline will first extract tearing mode freq and amp as 2 two fewatures,
+# this pipeline will first extract tearing mode freq and amp as 2 two features,
 # the result is output to an new file repo,
 
 # then to another person begin to work on the repo
 # some signal is dropped the rest is resample to 1kHz, and normalized
-# last disruption label is added as a new signal "disruption"
+# last disruption label is added as a new signal "disruptivity"
 
 # this code relies on basic_processor.py, the tearing mode feature extractor is defined there.
 
@@ -25,5 +25,5 @@
 # %%
 # init the input filerepo
 # drop signal
-# resamlpe and normalization
-# 这里我有个小问题，就是processor的设计，process的时候，结果存了硬盘吗？还是没有，需要调save吗？
+# resample and normalization
+
--- Examples/code/basic_processor.py
+++ Examples/code/basic_processor.py
@@ -1,4 +1,4 @@
 # %%
 # Here is a processor example
 
-# if calculate poloidal tearning mode frequency and amplitude.
+# it calculates poloidal tearing mode frequency and amplitude.
