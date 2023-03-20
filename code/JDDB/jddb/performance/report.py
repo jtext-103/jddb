7369893b9eca91a9484d82c67ba82eb300b59774
Aurora <435924704@qq.com>
2023-03-20 13:30:47 +0800

    report: Modify the input requirement of add()

--- code/JDDB/jddb/performance/report.py
+++ code/JDDB/jddb/performance/report.py
@@ -44,31 +44,32 @@ class Report:
     def save(self):
         self.report.to_excel(self.report_csv_path, index=False)
 
-    def add(self, result_csv_path: str, model_name: List[str], tardy_alarm_threshold, lucky_guess_threshold):
-        self.report_file()
+    def add(self, result_csv_path: str, model_name: str, tardy_alarm_threshold, lucky_guess_threshold):
+
         result = Result(result_csv_path)
         self.check_repeat(model_name)
+
         result.tardy_alarm_threshold = tardy_alarm_threshold
         result.lucky_guess_threshold = lucky_guess_threshold
-        result.get_y()
+        result.calc_metrics()
         tpr, fpr = result.ture_positive_rate()
         accuracy = result.get_accuracy()
         precision = result.get_precision()
         recall = result.get_recall()
         tp, fn, fp, tn = result.confusion_matrix()
-
+        index = len(self.report)
         self.report.loc[
-            len(self.report) + 1, ['model_name', 'accuracy', 'precision', 'recall', 'fpr', 'tpr', 'tp', 'fn', 'fp',
+            index, ['model_name', 'accuracy', 'precision', 'recall', 'fpr', 'tpr', 'tp', 'fn', 'fp',
                                    'tn']] = \
             [model_name, accuracy, precision, recall, tpr, fpr, tp, fn, fp, tn]
-        self.report.to_excel(self.report_csv_path, index=False)
+
 
     def remove(self, model_name: List[str]):
         model_name = self.check_no_exist(model_name)
         for i in range(len(model_name)):
             self.report = self.report.drop(self.report[self.report.model_name == model_name[i]].index)
 
-    def check_repeat(self, model_name: List[str]):
+    def check_repeat(self, model_name: str):
         err_list = []
         for i in range(len(model_name)):
             if model_name[i] in self.report.model_name.tolist():
@@ -89,4 +90,7 @@ class Report:
 # if __name__ == '__main__':
 #     report = Report("G:\datapractice\\test\\report.xlsx")
 #     report.report_file()
-#     report.add("G:\datapractice\\test\\test.xlsx", ["test"], 0.02, 0.3)
+#     report.add("G:\datapractice\\test\\test.xlsx", "test1", 0.02, 0.3)
+#     report.add("G:\datapractice\\test\\test.xlsx", "test2", 0.02, 0.3)
+#     report.save()
+
