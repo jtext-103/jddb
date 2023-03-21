59a433999994420621b4770dd0989e5eb76736c5
Aurora <435924704@qq.com>
2023-03-21 13:42:24 +0800

    revise roc() in order to show picture of roc-curve .

--- code/JDDB/jddb/performance/roc.py
+++ code/JDDB/jddb/performance/roc.py
@@ -1,15 +1,9 @@
-import xlwt
-import pandas as pd
-import os, sys
-from typing import Optional
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+import os
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import MultipleLocator
-import matplotlib
-from result import Result
 from report import Report
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import auc
+import matplotlib
 
 class Roc:
 
@@ -35,28 +29,28 @@ class Roc:
             tpr_ordered.append(tpr[i])
         fpr.sort()
         roc_auc = auc(fpr, tpr)
-        plt.figure()
         lw = 2
-        plt.figure(figsize=(10, 10))
+        matplotlib.use('QtAgg')
+        plt.rcParams["figure.figsize"] = (10, 10)
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
         plt.plot(np.array(fpr), np.array(tpr_ordered), color='darkorange',
-                 lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+                 lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
         plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
         plt.title('Receiver operating characteristic')
         plt.legend(loc="lower right")
         plt.savefig(os.path.join(roc_file_path, 'Receiver_operating_characteristic.png'), dpi=300)
-
+        plt.show(block=True)
 
 
 # if __name__ == '__main__':
-#     # report = Report("G:\datapractice\\test\\report.xlsx")
-#     # report.report_file()
-#     # report.add("G:\datapractice\\test\\test.xlsx", "test1", 0.02, 0.3)
-#     # report.add("G:\datapractice\\test\\test.xlsx", "test2", 0.02, 0.3)
-#     # report.save()
+#     report = Report("G:\datapractice\\test\\report.xlsx")
+#     report.report_file()
+# #     # report.add("G:\datapractice\\test\\test.xlsx", "test1", 0.02, 0.3)
+# #     # report.add("G:\datapractice\\test\\test.xlsx", "test2", 0.02, 0.3)
+# #     # report.save()
 #     roc = Roc("G:\datapractice\\test\\report.xlsx")
 #     roc.roc("G:\datapractice\\test")
 
