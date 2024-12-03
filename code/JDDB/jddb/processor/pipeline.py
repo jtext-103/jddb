87edf42086a78362ace410e1ea50adedda9a8a1f
Fengming Xue <1349976364@qq.com>
2024-12-03 16:19:53 +0800

    Realization of Pipeline.

--- code/JDDB/jddb/processor/pipeline.py
+++ code/JDDB/jddb/processor/pipeline.py
@@ -81,31 +81,105 @@ class Step:
 
 class Pipeline:
     def __init__(self, steps: List[Step] = None):
-        pass
+        self.steps = steps if steps else list()
 
     def add_step(self, step: Step):
-        pass
+        self.steps.append(step)
 
     def to_config(self) -> Dict:
-        pass
+        pipeline_config = dict()
+        for i, step in enumerate(self.steps):
+            pipeline_config[f'Step_{i + 1}'] = step.to_config()
+        return pipeline_config
 
     def to_yaml(self, filepath: str = 'pipeline_config.yaml'):
-        pass
+        pipeline_config = self.to_config()
+        with open(filepath, 'w') as file:
+            yaml.dump(pipeline_config, file)
 
     @classmethod
-    def from_config(cls, pipeline_config: Dict):
-        pass
+    def from_config(cls, pipeline_config: Dict, processor_registry: Dict):
+        steps = list()
+        for i in range(len(pipeline_config)):
+            steps.append(Step.from_config(pipeline_config[f'Step_{i + 1}'], processor_registry))
+        return cls(steps=steps)
 
     @classmethod
-    def from_yaml(cls, filepath: str):
-        pass
+    def from_yaml(cls, filepath: str, processor_registry: Dict):
+        with open(filepath, 'r') as file:
+            pipeline_config = yaml.safe_load(file)
+
+        return cls.from_config(pipeline_config=pipeline_config, processor_registry=processor_registry)
 
     def process_by_shot(self, shot: Shot, save_repo: FileRepo = None):
-        pass
+        logger = logging.getLogger('single_process')
+        logger.setLevel(logging.ERROR)
+        if not logger.hasHandlers():
+            file_handler = logging.FileHandler('./process_exceptions.log', mode="a")
+            file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+            logger.addHandler(file_handler)
+
+        for i, step in enumerate(self.steps):
+            try:
+                step.execute(shot)
+            except Exception as e:
+                error_message = (
+                    f"Shot No: {shot.shot_no}\n"
+                    f"Pipeline Step: {i + 1}\n"
+                    f"Processor Type: {type(step.processor).__name__}\n"
+                    f"Input Signals: {step.input_tags}\n"
+                    f"Output Signals: {step.output_tags}\n"
+                    f"Error Message: {e}\n"
+                )
+                logger.error(msg=error_message, exc_info=None)
+
+        shot.save(save_repo)
 
     def process_by_shotset(self, shotset: ShotSet, processes: int = mp.cpu_count() - 1,
                            shot_filter: List[int] = None, save_repo: FileRepo = None):
-        pass
+        """Process all shots in a ShotSet."""
+        if shot_filter is None:
+            shot_filter = shotset.shot_list
+        if processes:
+            if platform.system() == 'Linux':
+                pool = mp.get_context('spawn').Pool(processes=processes)
+            else:
+                pool = mp.Pool(processes=processes)
+            logger = logging.getLogger('multi_process')
+            logger.setLevel(logging.ERROR)
+            queue = mp.Manager().Queue(-1)
+            queue_handler = QueueHandler(queue)
+            logger.addHandler(queue_handler)
+            listener = QueueListener(
+                queue,
+                FileHandler('./process_exceptions.log', mode="a")
+            )
+            listener.start()
+            pool.map(partial(self._subprocess_task_in_shotset, queue=queue, shot_set=shotset, save_repo=save_repo),
+                     shot_filter)
+            pool.close()
+            pool.join()
+            listener.stop()
+        else:
+            for shot_no in shot_filter:
+                shot = shotset.get_shot(shot_no)
+                self.process_by_shot(shot, save_repo=save_repo)
 
     def _subprocess_task_in_shotset(self, shot_no: int, queue: Queue, shot_set: ShotSet, save_repo: FileRepo = None):
-        pass
+        single_shot = shot_set.get_shot(shot_no)
+        for i, step in enumerate(self.steps):
+            try:
+                step.execute(single_shot)
+            except Exception as e:
+                error_message = (
+                    f"Shot No: {shot_no}\n"
+                    f"Pipeline Step: {i + 1}\n"
+                    f"Processor Type: {type(step.processor).__name__}\n"
+                    f"Input Signals: {step.input_tags}\n"
+                    f"Output Signals: {step.output_tags}\n"
+                    f"Error Message: {e}\n"
+                )
+                queue.put(
+                    logging.LogRecord('multi_process', logging.ERROR, '', 0, error_message, exc_info=None, args=None))
+
+        single_shot.save(save_repo)
